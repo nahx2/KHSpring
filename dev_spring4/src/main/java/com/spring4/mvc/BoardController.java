8331267e6ed417d87751a6f3cd4aaaae3ced41f7
nahx2 <nax2boo@gmail.com>
2022-09-21 00:11:22 +0900

    dev_spring 수업 소스

--- dev_spring4/src/main/java/com/spring4/mvc/BoardController.java
+++ dev_spring4/src/main/java/com/spring4/mvc/BoardController.java
@@ -8,62 +8,78 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import com.google.gson.Gson;
+import com.util.HashMapBinder;
 
-//어노테이션 사용하지 않는다
+//여기선 어노테이션 사용하지 않는다.
 //@RestController
-public class BoardController extends MultiActionController {
+public class BoardController  extends MultiActionController{
 	Logger logger = Logger.getLogger(BoardController.class);
 	private BoardLogic boardLogic = null;
 	public void setBoardLogic(BoardLogic boardLogic) {
 		this.boardLogic = boardLogic;
 	}
 	public String jsonBoardList(HttpServletRequest req, HttpServletResponse res) {
-		logger.info("jsonBoardList 호출 성공");
+		logger.info("boardList호출 성공");
 		Map<String,Object> pMap = new HashMap<>();//@RequestParam Map<String,Object> pMap
 		List<Map<String,Object>> boardList = null; 
 		boardList = boardLogic.boardList(pMap);
 		//model.addAttribute("boardList", boardList);//scope:request
 		req.setAttribute("boardList", boardList);
-
 		logger.info(boardList);
 		return "forward:jsonBoardList.jsp";
 	}
 	public String boardList(HttpServletRequest req, HttpServletResponse res) {
-		logger.info("boardList 호출 성공");
-		Map<String,Object> pMap = new HashMap<>(); //@ReqestParam Map<String,Object> pMap
-		List<Map<String,Object>> boardList = null;
-		boardList	=	boardLogic.boardList(pMap); //scope:request
+		logger.info("boardList호출 성공");
+		Map<String,Object> pMap = new HashMap<>();//@RequestParam Map<String,Object> pMap
+		List<Map<String,Object>> boardList = null; 
+		boardList = boardLogic.boardList(pMap);
+		//model.addAttribute("boardList", boardList);//scope:request
 		req.setAttribute("boardList", boardList);
 		logger.info(boardList);
-		return "forward:list.jsp";
+		return "forward:boardList.jsp";
 	}
 	public String boardDetail(HttpServletRequest req, HttpServletResponse res) {
 		logger.info("boardDetail 호출 성공");
 		Map<String,Object> pMap = new HashMap<>();
-		boardLogic.boardDetail(pMap);
+		HashMapBinder hmb = new HashMapBinder(req);
+		hmb.bind(pMap);
+		List<Map<String,Object>> boardList = null; 
+		boardList = boardLogic.boardDetail(pMap);
+		req.setAttribute("boardList", boardList);
 		return "forward:read.jsp";
 	}
 	public String boardInsert(HttpServletRequest req, HttpServletResponse res) {
 		logger.info("boardInsert 호출 성공");
 		Map<String,Object> pMap = new HashMap<>();
+		HashMapBinder hmb = new HashMapBinder(req);
+		//hmb.bind(pMap);
+		hmb.multiBind(pMap);
 		boardLogic.boardInsert(pMap);
-		//redirect - forward, forward-> forward(에러)
+		//redirect-forward, forward->forward(에러)
 		return "redirect:boardList.sp";
 	}
 	public String boardUpdate(HttpServletRequest req, HttpServletResponse res) {
 		logger.info("boardUpdate 호출 성공");
 		Map<String,Object> pMap = new HashMap<>();
+		HashMapBinder hmb = new HashMapBinder(req);
+		hmb.bind(pMap);
 		boardLogic.boardUpdate(pMap);
+		//redirect-forward, forward->forward(에러)
 		return "redirect:boardList.sp";
 	}
 	public String boardDelete(HttpServletRequest req, HttpServletResponse res) {
 		logger.info("boardDelete 호출 성공");
 		Map<String,Object> pMap = new HashMap<>();
+		HashMapBinder hmb = new HashMapBinder(req);
+		hmb.bind(pMap);
 		boardLogic.boardDelete(pMap);
+		//redirect-forward, forward->forward(에러)
 		return "redirect:boardList.sp";
 	}
+
 }
